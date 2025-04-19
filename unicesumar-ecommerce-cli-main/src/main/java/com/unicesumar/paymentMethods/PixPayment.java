3c5f35c569bbf238a81a0b21411f99e034bc6565
naamasetogutte <160291641+naamasetogutte@users.noreply.github.com>
2025-04-18 21:12:00 -0300

    Delete unicesumar-ecommerce-cli-main directory

--- unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/paymentMethods/PixPayment.java
+++
@@ -1,7 +0,0 @@
-package com.unicesumar.paymentMethods;
-
-public class PixPayment implements PaymentMethod {
-    public void pay(double amount) {
-        System.out.println("Pagamento efetuado com sucesso via PIX");
-    }
-}
